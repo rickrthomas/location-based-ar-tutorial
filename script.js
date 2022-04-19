5e5d848e0d94431ba504bf1577db26e7f1ad71e4
rickrthomas <slocalrick@gmail.com>
2022-04-19 13:34:52 -0700

    bean

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '2 2 2',
+        scale: '0.001 0.001 0.001',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
     {
         url: './assets/articuno/pinetree.gltf',
-        scale: '1 1 1',
+        scale: '0.001 0.001 0.001',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
