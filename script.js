dcd506364ef284cd9033f753d51fc0d615e07a25
rickrthomas <slocalrick@gmail.com>
2022-04-19 12:59:29 -0700

    bean

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '0.3 0.3 0.3',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
